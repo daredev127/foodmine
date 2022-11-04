73ac7218371f503ff6ce6a2aa00d1c5537763974
daredev127 <francisdar127@gmail.com>
2022-11-04 15:35:26 +1100

    showed toast messages on login. also implemented logout

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,15 +13,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
-  loginForm!: FormGroup;
+  loginForm!: UntypedFormGroup;
   isSubmitted = false;
-  constructor(private formBuilder: FormBuilder) {}
+  returnUrl = '';
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
   }
 
   get fc() {
@@ -25,5 +39,14 @@ export class LoginPageComponent implements OnInit {
   submit() {
     this.isSubmitted = true;
     if (this.loginForm.invalid) return;
+
+    this.userService
+      .login({
+        email: this.fc.email.value,
+        password: this.fc.password.value,
+      })
+      .subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 }
