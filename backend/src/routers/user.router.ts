7ecf8c72dda4bae51378264d8ed2f43b06be55cc
daredev127 <francisdar127@gmail.com>
2022-11-10 08:27:14 +1100

    implemented the registration api

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { sample_users } from "../data";
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -29,8 +31,36 @@ router.post(
     if (user) {
       res.send(generateTokenResponse(user));
     } else {
-      res.status(400).send("Email or password is not valid!");
+      res.status(HTTP_BAD_REQUEST).send("Email or password is not valid!");
     }
+  })
+);
+
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password, address } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      res
+        .status(HTTP_BAD_REQUEST)
+        .send("User already exists. Please login instead.");
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+    };
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
   })
 );
 
