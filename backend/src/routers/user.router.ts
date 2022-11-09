6e87591ad8705a79f595d8ca453eff1e85ff0424
daredev127 <francisdar127@gmail.com>
2022-11-09 22:41:58 +1100

    connected the apis to the mongodb database

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,21 +1,38 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { sample_users } from "../data";
+import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/user.model";
 
 const router = Router();
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done!");
+      return;
+    }
 
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("Email or password is not valid!");
-  }
-});
+    await UserModel.create(sample_users);
+    res.send("Seed is done!");
+  })
+);
+
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      res.status(400).send("Email or password is not valid!");
+    }
+  })
+);
 
 const generateTokenResponse = (user: any) => {
   const token = jwt.sign(
