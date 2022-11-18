cbfceb1834d08ad3f893aff2b9dc112d02f4ecf1
daredev127 <francisdar127@gmail.com>
2022-11-18 18:33:22 +1100

    added the paypal button in the payment page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { OrderItemsListComponent } from './components/partials/order-items-list/
 import { MapComponent } from './components/partials/map/map.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
+import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import { PaymentPageComponent } from './components/pages/payment-page/payment-pa
     OrderItemsListComponent,
     MapComponent,
     PaymentPageComponent,
+    PaypalButtonComponent,
   ],
   imports: [
     BrowserModule,
